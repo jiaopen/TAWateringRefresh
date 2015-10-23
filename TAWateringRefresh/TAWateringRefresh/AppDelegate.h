46ca2cd2b07f59184c6e2f6fae5e84404ffe77ba
zip lee <54459708@qq.com>
2015-10-23 14:19:07 +0800

    init

---
+++ TAWateringRefresh/TAWateringRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TAWateringRefresh
+//
+//  Created by 李小盆 on 15/10/23.
+//  Copyright © 2015年 Zip Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TAWateringRefresh/TAWateringRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TAWateringRefresh
+//
+//  Created by 李小盆 on 15/10/23.
+//  Copyright © 2015年 Zip Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
